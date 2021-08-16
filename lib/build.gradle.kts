b2255c7a0fea7d81f7b14a9abb5e571fc84ebe5d
Patrick Pamponet Steiger <psteiger@gmail.com>
2021-08-15 21:57:51 -0300

    Fix lib name conflicting with project name

---
+++ lib/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    `maven-publish`
+}
+
+android {
+    compileSdk = 30
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 30
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:28.3.0"))
+    implementation("com.google.firebase:firebase-database")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            register("release", MavenPublication::class) {
+                from(components["release"])
+                artifactId = project.name
+            }
+        }
+    }
+}
